13eef13eb774627f9de6d33680422372ba16ce3b
Liz Johnson <liz.jade.johnson@gmail.com>
2024-06-01 11:07:45 -0400

    changing urls yet againnnnn

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,14 @@
+// const isDevelopment = import.meta.env.MODE === 'development';
+// const apiUrl = isDevelopment ? 'http://localhost:3000/api' : import.meta.env.VITE_API_URL;
+// console.log('Environment Mode:', import.meta.env.MODE);
+// console.log('API URL:', apiUrl);
+// export default apiUrl;
+
+
+
+
 const isDevelopment = import.meta.env.MODE === 'development';
-const apiUrl = isDevelopment ? 'http://localhost:3000/api' : import.meta.env.VITE_API_URL;
-console.log('Environment Mode:', import.meta.env.MODE);
-console.log('API URL:', apiUrl);
+const apiUrl = isDevelopment ? 'http://localhost:3000/api' : `${import.meta.env.VITE_API_URL}/api`;
+console.log('Environment Mode:', import.meta.env.MODE);  // Should output 'development' in local dev
+console.log('API URL:', apiUrl);  // Should output 'http://localhost:3000/api' in local dev
 export default apiUrl;
