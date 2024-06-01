362492ecf21863d450785c0e182f6084f240d51a
Liz Johnson <liz.jade.johnson@gmail.com>
2024-06-01 12:16:36 -0400

    changing env variables

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,14 +1,5 @@
-// const isDevelopment = import.meta.env.MODE === 'development';
-// const apiUrl = isDevelopment ? 'http://localhost:3000/api' : import.meta.env.VITE_API_URL;
-// console.log('Environment Mode:', import.meta.env.MODE);
-// console.log('API URL:', apiUrl);
-// export default apiUrl;
-
-
-
-
 const isDevelopment = import.meta.env.MODE === 'development';
-const apiUrl = isDevelopment ? 'http://localhost:3000/api' : `${import.meta.env.VITE_API_URL}/api`;
+const apiUrl = isDevelopment ? 'http://localhost:3000/api' : `${import.meta.env.VITE_API_URL}`;
 console.log('Environment Mode:', import.meta.env.MODE);  // Should output 'development' in local dev
 console.log('API URL:', apiUrl);  // Should output 'http://localhost:3000/api' in local dev
 export default apiUrl;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,26 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode (development, production, etc.)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': {
+        VITE_API_URL: env.VITE_API_URL
+      }
+    },
+    server: {
+      port: 5000,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
-  }
+  };
 });
