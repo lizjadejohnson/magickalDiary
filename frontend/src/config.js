4aacd74f489e7290fa9c195ac9a03c17449c2852
Liz Johnson <liz.jade.johnson@gmail.com>
2024-06-01 12:31:48 -0400

    changed cookie settings

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,6 @@
+//In development (isDevelopment is true), apiUrl will be http://localhost:3000/api
+//In production, apiUrl will be https://magickal-diary-backend.onrender.com/api (because we'll have that set in Render)
+
 const isDevelopment = import.meta.env.MODE === 'development';
 const apiUrl = isDevelopment ? 'http://localhost:3000/api' : `${import.meta.env.VITE_API_URL}`;
 console.log('Environment Mode:', import.meta.env.MODE);  // Should output 'development' in local dev
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -24,3 +24,14 @@ export default defineConfig(({ mode }) => {
     }
   };
 });
+
+//The proxy configuration in Vite is used to make API calls from the frontend development server (running on port 5000)
+//to the backend server (running on port 3000).
+
+// When you make a request to /api from your frontend, Vite will proxy this request to http://localhost:3000.
+// The rewrite: (path) => path.replace(/^\/api/, '') part makes it so that the /api prefix is removed before the request is sent to http://localhost:3000.
+
+//The frontend is served on http://localhost:5000.
+// The backend is served on http://localhost:3000/api.
+// The Vite proxy redirects all /api calls from the frontend to the backend,
+//making the frontend *think* it's calling http://localhost:5000/api, while it's actually hitting http://localhost:3000.
