9331faa7338b6b5b1b41d980534498acba9431e6
octophetus <octophetus@gmail.com>
2024-05-31 23:24:03 -0400

    changes for Render

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-const apiUrl = import.meta.env.VITE_API_URL;
+const isDevelopment = import.meta.env.MODE === 'development';
+const apiUrl = isDevelopment ? '/api' : import.meta.env.VITE_API_URL;
 export default apiUrl;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5000
+    port: 5000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  build: {
+    outDir: 'dist'
   }
 })
