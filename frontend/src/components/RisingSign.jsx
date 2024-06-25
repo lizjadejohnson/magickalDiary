8ef23bf00909e7ecc22de3a6c6de5e7192a7a5b4
Liz Johnson <liz.jade.johnson@gmail.com>
2024-06-25 11:32:34 -0400

    rising sign tab working

--- frontend/src/components/RisingSign.jsx
+++ frontend/src/components/RisingSign.jsx
@@ -1,35 +1,17 @@
 import React from 'react';
+import {PlanetaryMeaning} from '../../utilities/ephemerisHelper.jsx';
 
-const RisingSign = ({ planets }) => {
-
-    if (!planets) {
-        return (
-            <Spinner
-            redirectTo={"/edit-profile"}
-            delay={5000}
-            message={"To access Ephemeris data, you must have accurate time and location of birth saved. Redirecting to edit profile page..."}
-            />
-        );
-    }
-
-  const risingData = planets['Ascendant'];
-  return (
-    <>
-        <div className='zodiaccard-container'>
-            <h2 className='zodiaccard-header'>Ascendant/Rising Sign ↗</h2>
-            <div className='zodiaccard-body'>
-                <p>Outer personality and how you present yourself to the world.</p>
-                <p>The Ascendant is the sign that was rising on the eastern horizon at the time of your birth. It influences your physical appearance, demeanor, and how others perceive you. It also sets the stage for the entire birth chart by determining the house cusps.</p>
-            </div>
-        </div>
-        <div className='zodiaccard-container'>
-            <h3 className='zodiaccard-header'>{risingData ? risingData.formattedPosition : 'No rising sign data available.'}</h3>
-            <div className='zodiaccard-body'>
-                <p>IMPORT RISING SIGN MEANING HERE</p>
-            </div>
-        </div>
-    </>
-  );
-};
+const RisingSign = ({ planets }) => (
+  <div>
+    <div className='zodiaccard-container'>
+      <h2 className='zodiaccard-header'>Ascendant (Rising Sign) ↗</h2>
+      <div className='zodiaccard-body'>
+        <p>Outer personality and how you present yourself to the world.</p>
+        <p>The Ascendant is the sign that was rising on the eastern horizon at the time of your birth. It influences your physical appearance, demeanor, and how others perceive you. It also sets the stage for the entire birth chart by determining the house cusps.</p>
+      </div>
+    </div>
+    <PlanetaryMeaning planet="Ascendant" planets={planets} />
+  </div>
+);
 
 export default RisingSign;
