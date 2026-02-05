5418572daa04675c41b4393fed8eb4235d0216a1
lizjadejohnson <liz.jade.johnson@gmail.com>
2026-02-05 01:08:20 -0500

    updated rtarot routes

--- backend/routes/meaningsRoutes.js
+++ backend/routes/meaningsRoutes.js
@@ -3,18 +3,17 @@ const router = express.Router();
 const meaningsController = require('../controllers/meaningsController')
 const authenticate = require('../config/jwtAuth.js');
 
-//-------------------------TODO ROUTES-------------------------
+//-------------------------MEANINGS ROUTES-------------------------
 // -----Get ALL Meanings (GET):
 router.get("/", authenticate, meaningsController.fetchAllMeanings)
 
-// -----Get specific Meanings by ID (GET):
-router.get("/:id", authenticate, meaningsController.fetchMeaning)
+// Get all Tarot cards (GET) - MUST BE BEFORE /:id
+router.get("/tarot/all", authenticate, meaningsController.fetchAllTarotCards);
 
 // Get Meaning by the hexagram lines (POST)
 router.post("/by-lines", authenticate, meaningsController.fetchMeaningByLines);
 
-// Get all Tarot cards (GET)
-router.get("/tarot/all", authenticate, meaningsController.fetchAllTarotCards);
-
+// -----Get specific Meanings by ID (GET): - MUST BE LAST
+router.get("/:id", authenticate, meaningsController.fetchMeaning)
 
 module.exports = router;
